d81239ed12a604a2800c545ea2f0f076c36c0456
Nani500411 <82075407+Nani500411@users.noreply.github.com>
2021-04-07 11:24:51 +0530

    Tools

--- index.js
+++ index.js
@@ -1,105 +1,265 @@
-const { chalk, inquirer, print } = require("./tools/index.js");
-var moment = require("moment");
-var colors = require("colors");
-var userHome = require("user-home");
+const { IgApiClient } = require("instagram-private-api");
+const rp = require("request-promise");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const _ = require("lodash");
+const fs = require("fs");
 
-//DETECT IP *START!
-var os = require("os");
-var interfaces = os.networkInterfaces();
-var addresses = [];
-for (var k in interfaces) {
-  for (var k2 in interfaces[k]) {
-    var address = interfaces[k][k2];
-    if (address.family === "IPv4" && !address.internal) {
-      addresses.push(address.address);
+const ig = new IgApiClient();
+
+class instagram {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
+
+  async login() {
+    ig.state.generateDevice(this.username);
+    try {
+      const login = await ig.account.login(this.username, this.password);
+      return Promise.resolve(login);
+    } catch (err) {
+      if (err.message.match(/challenge_required/i)) {
+        console.log(chalk`{yellow [!] Challenge required!}`);
+        try {
+          await ig.challenge.auto(true);
+          const { code } = await inquirer.prompt({
+            type: "input",
+            name: "code",
+            message: "⌭ Input verification code (check it in email or sms):",
+            validate: (val) => /[0-9]/.test(val) || "Only input numbers!",
+          });
+          const verify = await ig.challenge.sendSecurityCode(code);
+          return Promise.resolve(verify.logged_in_user);
+        } catch (chErr) {
+          return Promise.reject(chErr.message);
+        }
+      } else return Promise.reject(err.message);
+    }
+  }
+
+  async getIdByUsername(username) {
+    try {
+      const id = await ig.user.getIdByUsername(username);
+      return Promise.resolve(id);
+    } catch (err) {
+      return Promise.reject(err);
+    }
+  }
+
+  async userInfo(uid) {
+    try {
+      const info = await ig.user.info(uid);
+      return Promise.resolve(info);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async mediaInfo(mid) {
+    try {
+      const info = await ig.media.info(mid);
+      return Promise.resolve(info);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async friendshipStatus(uid) {
+    try {
+      const status = ig.friendship.show(uid);
+      return Promise.resolve(status);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async userFeed(uid) {
+    try {
+      const feed = await ig.feed.user(uid);
+      return Promise.resolve(feed);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async timelineFeed() {
+    try {
+      const feed = await ig.feed.timeline();
+      return Promise.resolve(feed);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async userReels(uid) {
+    try {
+      const reels = await ig.feed.reelsMedia({ userIds: [uid] });
+      return Promise.resolve(reels);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async timelineReels() {
+    try {
+      const reels = await ig.feed.reelsTray();
+      return Promise.resolve(reels);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async locationFeed(lid) {
+    try {
+      const feed = await ig.feed.location(lid);
+      return Promise.resolve(feed);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async tagFeed(hashtag) {
+    try {
+      const feed = await ig.feed.tag(hashtag);
+      return Promise.resolve(feed);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async followersFeed(uid) {
+    try {
+      const followers = await ig.feed.accountFollowers(uid);
+      return Promise.resolve(followers);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async followingFeed(uid) {
+    try {
+      const following = await ig.feed.accountFollowing(uid);
+      return Promise.resolve(following);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async getMediaIdByUrl(url) {
+    try {
+      const get = await rp({
+        url: `http://api.instagram.com/oembed?url=${url}`,
+        method: "GET",
+        json: true,
+      });
+      const id = get.media_id.split("_")[0] || "";
+      return Promise.resolve(id);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async getMediaLikers(mid) {
+    try {
+      const get = await ig.media.likers(mid);
+      return Promise.resolve(get);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async follow(uid) {
+    try {
+      await ig.friendship.create(uid);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async unfollow(uid) {
+    try {
+      await ig.friendship.destroy(uid);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async like(mid) {
+    try {
+      await ig.media.like({
+        mediaId: mid,
+        moduleInfo: { module_name: "profile" },
+      });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async unlike(mid) {
+    try {
+      await ig.media.unlike({
+        mediaId: mid,
+        moduleInfo: { module_name: "profile" },
+      });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async comment(mid, msg) {
+    try {
+      await ig.media.comment({ mediaId: mid, text: msg });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async deleteMedia(mid, type) {
+    try {
+      const del = await ig.media.delete({
+        mediaId: mid,
+        mediaType: type.toUpperCase(),
+      });
+      const isOk = del.did_delete ? true : false;
+      return Promise.resolve(isOk);
+    } catch (err) {
+      return Promise.reject(err.message);
+    }
+  }
+
+  async markStorySeen(item) {
+    try {
+      await ig.story.seen([item]);
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async sendDirectMessage(tid, msg) {
+    try {
+      const thread = ig.entity.directThread([tid.toString()]);
+      await thread.broadcastText(msg);
+      return true;
+    } catch (err) {
+      return false;
     }
   }
 }
-//DETECT IP *END!
-const questionTools = [
-  "➥ Information",
-  "➥ Bot Like Timeline",
-  "➥ Bot Like Target User",
-  "➥ Mass Delete Post/Photo",
 
-  "➥ F-L-C -> Followers Target",
-  "➥ F-L-C -> Followers Target [BETA]",
-
-  "➥ F-L-C -> Followers Target v2",
-
-  "➥ F-L-DM -> Followers Target",
-  "➥ F-L-DM -> Followers Target [BETA]",
-
-  "➥ F-L-C -> Hashtag Target",
-  "➥ F-L-C -> Location Target",
-
-  "➥ Unfollow All Following",
-  "➥ Unfollow Not Followback",
-  "\n",
-];
-
-const menuQuestion = {
-  type: "list",
-  name: "choice",
-  message:
-    "Select tools:\n  Read the (❆ Information) first before using the tool! Nyaa~\n\n",
-  choices: questionTools,
+const print = (msg, type, line) => {
+  !type && console.log(msg);
+  type == "ok" && console.log(chalk`{green ${line ? "\n" : ""}⊙ ${msg}}`);
+  type == "wait" && console.log(chalk`{bold.cyan ${line ? "\n" : ""}∞ ${msg}}`);
+  type == "warn" && console.log(chalk`{yellow ${line ? "\n" : ""}≉ ${msg}}`);
+  type == "err" && console.log(chalk`{red ${line ? "\n" : ""}⋈ ${msg}}`);
 };
 
-const main = async () => {
-  try {
-    const { choice } = await inquirer.prompt(menuQuestion);
-    choice == questionTools[0] && require("./tools/info.js");
-    choice == questionTools[1] && require("./tools/liketimeline.js");
-    choice == questionTools[2] && require("./tools/liketarget.js");
-    choice == questionTools[3] && require("./tools/delallmedia.js");
-    choice == questionTools[4] && require("./tools/fftauto.js");
-    choice == questionTools[5] && require("./tools/fftbetaauto.js");
-    choice == questionTools[6] && require("./tools/fftautov2.js");
-    choice == questionTools[7] && require("./tools/fftdmauto.js");
-    choice == questionTools[8] && require("./tools/fftdmbetaauto.js");
-    choice == questionTools[9] && require("./tools/fhtauto.js");
-    choice == questionTools[10] && require("./tools/fltauto.js");
-    choice == questionTools[11] && require("./tools/unfollowall.js");
-    choice == questionTools[12] && require("./tools/unfollnotfollback.js");
-    choice == questionTools[13] && process.exit();
-  } catch (err) {
-    print(err, "err");
-  }
-};
-
-console.log(chalk`{bold.green
-  
-▀█▀ █▀▀▄ ▀▀█▀▀ █▀▀ █▀▀█ █▀▀▄ █▀▀ ▀▀█▀▀ 
-▒█░ █░░█ ░░█░░ █▀▀ █▄▄▀ █░░█ █▀▀ ░░█░░ 
-▄█▄ ▀░░▀ ░░▀░░ ▀▀▀ ▀░▀▀ ▀░░▀ ▀▀▀ ░░▀░░ 
-
-█░░░█ █▀▀█ █▀▀█ █░░ █▀▀▄ 
-█▄█▄█ █░░█ █▄▄▀ █░░ █░░█ 
-░▀░▀░ ▀▀▀▀ ▀░▀▀ ▀▀▀ ▀▀▀░
-
-  
-  
-
-
-  Ξ TITLE  : internetworld
-  Ξ UPDATE : 08/03/2021#12:00
-  Ξ EMAIL  : smail500411@gmail.com
-
-  116 111 111 108 115 105 103  118 51 
-  }`);
-console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
-console.log(
-  "  Ξ START  : ".bold.red + moment().format("D MMMM YYYY, h:mm:ss a")
-);
-console.log("  Ξ YPATH  : ".bold.red + userHome);
-console.log("  Ξ YOUIP  : ".bold.red + addresses);
-console.log(chalk`{bold.red   •••••••••••••••••••••••••••••••••••••••••}`);
-console.log(chalk`{bold.yellow
-  Github ••••••••••••••••••••••••••••••••••
-  : @nani_500411
-  : @internet_world_01
-  •••••••••••••••••••••••••••••••••••••••••
-  : Neelakanta
-  : Thanks To All Member Sharing Gils Blog }\n`);
-main();
+const delay = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+module.exports = { chalk, inquirer, _, fs, instagram, print, delay };
